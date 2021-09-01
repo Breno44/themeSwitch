08966586868ccab72f6670f06f0fde130a2e1524
Breno Henrique <bhfs.2908@gmail.com>
2021-09-01 15:20:34 -0300

    feat:ending project

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import Header from "./components/Header";
 import GlobalStyle from "./styles/global";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider, DefaultTheme } from "styled-components";
 import light from "./styles/themes/light";
+import dark from "./styles/themes/dark";
+import usePersistedState from "./utils/usePersistedState";
 
 const App = () => {
+  const [theme, setTheme] = usePersistedState<DefaultTheme>("theme", light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === "light" ? dark : light);
+  };
+
   return (
-    <ThemeProvider theme={light}>
-      <div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
         <GlobalStyle />
-        <Header />
+        <Header toogleTheme={toggleTheme} />
       </div>
     </ThemeProvider>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Container } from "./styles";
+import { ThemeContext } from "styled-components";
 import Switch from "react-switch";
+import { shade } from "polished";
 
-const Header: React.FC = () => {
+interface Props {
+  toogleTheme(): void;
+}
+
+const Header: React.FC<Props> = ({ toogleTheme }) => {
+  const { colors, title } = useContext(ThemeContext);
+
   return (
     <Container>
       Hello world!
-      <Switch onChange={() => {}} checked={true} checkedIcon={false} uncheckedIcon={false} height={40} width={40} />
+      <Switch
+        onChange={toogleTheme}
+        checked={title === "dark"}
+        checkedIcon={false}
+        uncheckedIcon={false}
+        height={10}
+        width={40}
+        handleDiameter={20}
+        offColor={shade(0.15, colors.primary)}
+        onColor={colors.secondary}
+      />
     </Container>
   );
 };
